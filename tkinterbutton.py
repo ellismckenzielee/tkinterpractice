66e8d205914412929330621658c4cdeb5b9c7742
Ellis Lee <ellismckenzielee@gmail.com>
2019-01-18 13:38:51 +0000

    completed checkbutton tutorial

--- tkinterbutton.py
+++ tkinterbutton.py
@@ -8,25 +8,21 @@ Created on Thu Jan 17 13:17:23 2019
 
 from tkinter import *
 from tkinter import ttk
+
 class Root(Tk):
     def __init__(self):
         super(Root, self).__init__()
         self.title('Combobox')
         self.minsize(640, 400)
         self.configure(background='white')
-        self.initUI()
-    
-    def clickMe(self):
-        self.label.configure(text='you have selected' + self.myfruit.get())
-    def initUI(self):
-        self.myfruit = StringVar()
-        self.combo = ttk.Combobox(self, width = 15, textvariable = self.myfruit)
-        self.combo['values'] = ('Apple', 'Pear')
-        self.combo.grid(column = 1,row = 1)
-        self.label = ttk.Label(self, text='select fruit')
-        self.label.grid(column=5, row = 5)
-        self.button = ttk.Button(self, text = 'click', command = self.clickMe)
-        self.button.grid(column=2, row = 1)
-        
+        self.checkButton()
+
+    def checkButton(self):
+        self.check1 = ttk.Checkbutton(self, text='Disabled', state='disabled')
+        self.check1.grid(column=0,row=0, sticky = W)
+        self.check2 = ttk.Checkbutton(self, text = 'unchecked')
+        self.check2.grid(row=0, column=1)
+        self.check3 = ttk.Checkbutton(self, text = 'enabled', state='enabled')
+        self.check3.grid(column = 3 , row = 1)
 root = Root()
 root.mainloop()
