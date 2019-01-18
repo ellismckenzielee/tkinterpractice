f8ba992b7ca1ee26f8e7a688368d6cbf58205070
Ellis Lee <ellismckenzielee@gmail.com>
2019-01-18 13:28:54 +0000

    completed combobox tutorial

--- tkinterbutton.py
+++ tkinterbutton.py
@@ -8,28 +8,25 @@ Created on Thu Jan 17 13:17:23 2019
 
 from tkinter import *
 from tkinter import ttk
-
 class Root(Tk):
     def __init__(self):
         super(Root, self).__init__()
-        self.title('Tkinter Button')
+        self.title('Combobox')
         self.minsize(640, 400)
         self.configure(background='white')
         self.initUI()
-        
+    
     def clickMe(self):
-        self.label.configure(text='Hello' + self.name.get())
-        self.label.configure(foreground = 'green')
-        
-        
-        
+        self.label.configure(text='you have selected' + self.myfruit.get())
     def initUI(self):
-        self.name = StringVar()
-        self.label = Label(self, text = 'Enter your name')
-        self.label.grid(column=0, row=0)
-        self.textbox = Entry(self, width=20, textvariable = self.name)
-        self.textbox.grid(column=0,row=1)
-        self.button = Button(self, text='Submit', command = self.clickMe)
-        self.button.grid(column=0,row=2)
+        self.myfruit = StringVar()
+        self.combo = ttk.Combobox(self, width = 15, textvariable = self.myfruit)
+        self.combo['values'] = ('Apple', 'Pear')
+        self.combo.grid(column = 1,row = 1)
+        self.label = ttk.Label(self, text='select fruit')
+        self.label.grid(column=5, row = 5)
+        self.button = ttk.Button(self, text = 'click', command = self.clickMe)
+        self.button.grid(column=2, row = 1)
+        
 root = Root()
 root.mainloop()
