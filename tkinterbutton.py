fc1f1709b9ccfacdc79fd039248a370258c641fb
Ellis Lee <ellismckenzielee@gmail.com>
2019-01-18 14:23:34 +0000

    radio button tutorial completed

--- tkinterbutton.py
+++ tkinterbutton.py
@@ -9,20 +9,37 @@ Created on Thu Jan 17 13:17:23 2019
 from tkinter import *
 from tkinter import ttk
 
+
+COLOR1 = 'Blue'
+COLOR2 = 'Red'
+COLOR3 = 'Gold'
+ 
 class Root(Tk):
     def __init__(self):
         super(Root, self).__init__()
-        self.title('Combobox')
+        self.title('Tkinter Radio Button')
         self.minsize(640, 400)
         self.configure(background='white')
-        self.checkButton()
-
-    def checkButton(self):
-        self.check1 = ttk.Checkbutton(self, text='Disabled', state='disabled')
-        self.check1.grid(column=0,row=0, sticky = W)
-        self.check2 = ttk.Checkbutton(self, text = 'unchecked')
-        self.check2.grid(row=0, column=1)
-        self.check3 = ttk.Checkbutton(self, text = 'enabled', state='enabled')
-        self.check3.grid(column = 3 , row = 1)
+        self.radioButton()
+    
+    def radioEvent(self):
+        radSelected = self.radValues.get()
+        
+        if radSelected == 1:
+            self.configure(background = COLOR1)
+        elif radSelected == 2:
+            self.configure(background = COLOR2)
+        elif radSelected == 3:
+            self.configure(background = COLOR3)
+        
+    def radioButton(self):
+        self.radValues = IntVar()
+        self.rad1 = ttk.Radiobutton(self, text=COLOR1, value=1, variable = self.radValues, command = self.radioEvent)
+        self.rad1.grid(column=0,row=0, sticky=W, columnspan = 3)
+        self.rad2 = ttk.Radiobutton(self, text=COLOR2, value=2, variable = self.radValues, command = self.radioEvent)
+        self.rad2.grid(column=0,row=1, sticky=W, columnspan = 3)
+        self.rad3 = ttk.Radiobutton(self, text=COLOR3, value=3, variable = self.radValues, command = self.radioEvent)
+        self.rad3.grid(column=0,row=2, sticky=W, columnspan = 3)
+        
 root = Root()
 root.mainloop()
