339d2c08887f03c28a3956c6691785841335f454
Ellis Lee <ellismckenzielee@gmail.com>
2019-01-22 14:34:35 +0000

    completed thenewboston Python GUI with Tkinter -5- More on the grid layout

--- thenewboston1.py
+++ thenewboston1.py
@@ -19,10 +19,16 @@ label_2 = Label(root, text='Password:')
 entry_1 = Entry(root)
 entry_2 = Entry(root)
 
-label_1.grid(row=0)
+
+#Add sticky and the direction to align text in frame/window/column
+label_1.grid(row=0, sticky=E)
 label_2.grid(row=1)
 entry_1.grid(row=0, column=1)
 entry_2.grid(row=1, column=1)
+
+#Checkbox
+c = Checkbutton(root, text='Keep me logged in')
+c.grid(row=2, columnspan=2)
 
 #Keeps the program running 
 root.mainloop()
