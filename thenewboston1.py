fdc1773301101c93b8d14f77b337942881ff77ce
Ellis Lee <ellismckenzielee@gmail.com>
2019-01-22 14:13:06 +0000

    completed thenewboston Python GUI with Tkinter -2- Organizing layout

--- thenewboston1.py
+++ thenewboston1.py
@@ -11,9 +11,24 @@ from tkinter import *
 #Creating a blank window, which is equal to root
 root = Tk()
 
-#AddLabels
-theLabel = Label(root, text='This is a label')
-theLabel.pack()
+
+#Split the window into two smaller frames (top and bottom)
+topFrame = Frame(root)
+topFrame.pack()
+
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+#Widgets
+button1 = Button(topFrame, text="Button 1", fg='red')
+button2 = Button(topFrame, text="Button 2", fg='blue')
+button3 = Button(topFrame, text="Button 3", fg='green')
+button4 = Button(bottomFrame, text="Button 4", fg='purple')
+
+button1.pack(side=LEFT)
+button2.pack(side=LEFT)
+button3.pack(side=LEFT)
+button4.pack()
 
 #Keeps the program running 
 root.mainloop()
