c9104c1dbe97c78aec03a7629a7d225cc673bdf1
Ellis Lee <ellismckenzielee@gmail.com>
2019-01-23 10:40:57 +0000

    completed thenewboston Python GUI with Tkinter -10- Creating a Toolbar

--- thenewboston1.py
+++ thenewboston1.py
@@ -13,9 +13,14 @@ def doNothing():
 
 root = Tk()
 
+# ******* The Main Menu ********
+
+#Add main menu to the root and config
+#By default this is at top of the screen
 menu = Menu(root)
 root.config(menu=menu)
 
+#Add items to the main menu
 subMenu = Menu(menu)
 menu.add_cascade(label='File', menu=subMenu)
 subMenu.add_command(label='New Project', command=doNothing)
@@ -30,5 +35,15 @@ menu.add_cascade(label='Edit', menu=editMenu)
 editMenu.add_command(label='Undo', command=doNothing)
 editMenu.add_command(label='Redo', command=doNothing)
 
+# ******* The toolbar ********
+toolbar = Frame(root, bg='black')
+
+insertButton = Button(toolbar, text='Insert Image', command=doNothing)
+insertButton.pack(side=LEFT, padx=2, pady=2)
+
+printButton = Button(toolbar, text='Print', command=doNothing)
+printButton.pack(side=LEFT, padx=2, pady=2)
+
+toolbar.pack(side=TOP, fill=X)
 
 root.mainloop()
